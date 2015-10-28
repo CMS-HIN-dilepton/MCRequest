706f2646f2769a4b167eec344174569d06f91446
cmironov <eusmartass@pb-d-128-141-138-25.cern.ch>
2015-10-28 18:04:32 +0100

    config files for the 2015 data, pp and embedded samples

---
+++ 2015/Psi2SMM_5p02TeV_TuneCUETP8M1_ptPsi69_cfi.py
@@ -0,0 +1,64 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         crossSection = cms.untracked.double(0),
+                         comEnergy = cms.double(5020.0),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'Charmonium:states(3S1) = 100443', # filter on 100443 and prevents other onium states decaying to 443, so we should turn the others off
+            'Charmonium:O(3S1)[3S1(1)] = 0.76',
+            'Charmonium:O(3S1)[3S1(8)] = 0.005',
+            'Charmonium:O(3S1)[1S0(8)] = 0.004',
+            'Charmonium:O(3S1)[3P0(8)] = 0.004',
+            'Charmonium:gg2ccbar(3S1)[3S1(1)]g = on',
+            'Charmonium:gg2ccbar(3S1)[3S1(8)]g = on',
+            'Charmonium:qg2ccbar(3S1)[3S1(8)]q = on',
+            'Charmonium:qqbar2ccbar(3S1)[3S1(8)]g = on',
+            'Charmonium:gg2ccbar(3S1)[1S0(8)]g = on',
+            'Charmonium:qg2ccbar(3S1)[1S0(8)]q = on',
+            'Charmonium:qqbar2ccbar(3S1)[1S0(8)]g = on',
+            'Charmonium:gg2ccbar(3S1)[3PJ(8)]g = on',
+            'Charmonium:qg2ccbar(3S1)[3PJ(8)]q = on',
+            'Charmonium:qqbar2ccbar(3S1)[3PJ(8)]g = on',
+            '100443:onMode = off',      # ignore cross-section re-weighting (CSAMODE=6) since selecting wanted decay mode 
+            '100443:onIfAny = 13 -13',
+            'PhaseSpace:pTHatMin = 2.'
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
+
+oniafilter = cms.EDFilter("PythiaFilter",
+    Status = cms.untracked.int32(2),
+    MaxEta = cms.untracked.double(1000.0),
+    MinEta = cms.untracked.double(-1000.0),
+    MinPt = cms.untracked.double(6.0),
+    MaxPt = cms.untracked.double(9.0),
+    ParticleID = cms.untracked.int32(100443)
+)
+
+mumugenfilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(1, 1),
+    MinPt = cms.untracked.vdouble(0.5, 0.5),
+    MinP = cms.untracked.vdouble(0., 0.),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    MinInvMass = cms.untracked.double(2.0),
+    MaxInvMass = cms.untracked.double(4.0),
+    ParticleCharge = cms.untracked.int32(-1),
+    ParticleID1 = cms.untracked.vint32(13),
+    ParticleID2 = cms.untracked.vint32(13)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*oniafilter*mumugenfilter)
