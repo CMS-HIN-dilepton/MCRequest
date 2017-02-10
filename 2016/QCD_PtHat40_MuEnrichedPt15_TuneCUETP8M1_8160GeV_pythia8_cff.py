317398ec611ed927887e42e2c076bb5c7af0c9e9
Mihee <mihee.jo@cern.ch>
2017-02-10 14:38:42 +0100

    QCD with long-lived hadrons(K0_L, pi+, K+) and muon genfilter

---
+++ 2016/QCD_PtHat40_MuEnrichedPt15_TuneCUETP8M1_8160GeV_pythia8_cff.py
@@ -0,0 +1,50 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+  maxEventsToPrint = cms.untracked.int32(0),
+  pythiaPylistVerbosity = cms.untracked.int32(0),
+  filterEfficiency = cms.untracked.double(1),
+  pythiaHepMCVerbosity = cms.untracked.bool(False),
+  comEnergy = cms.double(8160.0),
+  PythiaParameters = cms.PSet(
+    pythia8CommonSettingsBlock,
+    pythia8CUEP8M1SettingsBlock,
+    processParameters = cms.vstring(
+    'ParticleDecays:limitTau0 = off',
+    'ParticleDecays:limitCylinder = on',
+    'ParticleDecays:xyMax = 2000',
+    'ParticleDecays:zMax = 4000',
+    'HardQCD:all = on',
+    'PhaseSpace:pTHatMin = 40',
+    'PhaseSpace:pTHatMax = 9999',
+    '130:mayDecay = on',
+    '211:mayDecay = on',
+    '321:mayDecay = on'
+    ),
+    parameterSets = cms.vstring('pythia8CommonSettings',
+      'pythia8CUEP8M1Settings',
+      'processParameters',
+    )
+  )
+)
+
+mugenfilter = cms.EDFilter("MCSmartSingleParticleFilter",
+      MinPt = cms.untracked.vdouble(15.),
+      MinEta = cms.untracked.vdouble(-2.5),
+      MaxEta = cms.untracked.vdouble(2.5),
+      ParticleID = cms.untracked.vint32(13),
+      Status = cms.untracked.vint32(1),
+      # Decay cuts are in mm
+      MaxDecayRadius = cms.untracked.vdouble(2000.),
+      MinDecayZ = cms.untracked.vdouble(-4000.),
+      MaxDecayZ = cms.untracked.vdouble(4000.)
+)
+
+configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('PYTHIA 8 QCD in NN (pt-hat 40 GeV) at sqrt(s) = 8.16 TeV')
+)
+
+ProductionFilterSequence = cms.Sequence(generator*mugenfilter)
