02d53b2d8aa18f436417d2ba1203a5398e150007
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2019-05-21 22:31:47 +0200

    Add samples for Low PU

---
+++ LowPU/Ups3SMM/Ups3SMM_13TeV_TuneCP5_nofilter_cfi.py
@@ -0,0 +1,53 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(0.109),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         crossSection = cms.untracked.double(1430000.0),
+                         comEnergy = cms.double(13000.0),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            'Bottomonium:states(3S1) = 200553', # filter on 553 and prevents other onium states decaying to 553, so we should turn the others off
+            'Bottomonium:O(3S1)[3S1(1)] = 3.54',
+            'Bottomonium:O(3S1)[3S1(8)] = 0.075',
+            'Bottomonium:O(3S1)[1S0(8)] = 0.1',
+            'Bottomonium:O(3S1)[3P0(8)] = 0.1',
+            'Bottomonium:gg2bbbar(3S1)[3S1(1)]g = on',
+            'Bottomonium:gg2bbbar(3S1)[3S1(8)]g = on',
+            'Bottomonium:qg2bbbar(3S1)[3S1(8)]q = on',
+            'Bottomonium:qqbar2bbbar(3S1)[3S1(8)]g = on',
+            'Bottomonium:gg2bbbar(3S1)[1S0(8)]g = on',
+            'Bottomonium:qg2bbbar(3S1)[1S0(8)]q = on',
+            'Bottomonium:qqbar2bbbar(3S1)[1S0(8)]g = on',
+            'Bottomonium:gg2bbbar(3S1)[3PJ(8)]g = on',
+            'Bottomonium:qg2bbbar(3S1)[3PJ(8)]q = on',
+            'Bottomonium:qqbar2bbbar(3S1)[3PJ(8)]g = on',
+            'Bottomonium:gg2bbbar(3S1)[3S1(1)]gm = on',
+            '200553:onMode = off',            # ignore cross-section re-weighting (CSAMODE=6) since selecting wanted decay mode
+            '200553:onIfAny = 13 -13',
+            'PhaseSpace:pTHatMin = 2.',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+        )
+)
+
+# Next two muon filter are derived from muon reconstruction
+oniafilter = cms.EDFilter("PythiaFilter",
+    Status = cms.untracked.int32(2),
+    MaxEta = cms.untracked.double(1000.0),
+    MinEta = cms.untracked.double(-1000.0),
+    MinPt = cms.untracked.double(0.0),
+    ParticleID = cms.untracked.int32(200553)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*oniafilter)
+
