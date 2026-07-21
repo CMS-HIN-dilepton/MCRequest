6bfb92bd384ce32cb35f45c80ff28a281fcba9c8
arswain875 <arunima.swain@cern.ch>
2026-07-21 19:10:56 +0530

    Update filter efficiency and cross section values
    Removing Mother Eta cut

--- Run3/Xi_Omega_config/OmegaToLambdaKaon_5p36TeV_Pythia8TuneCP5_EvtGen_cfi.py.py
+++ Run3/Xi_Omega_config/OmegaToLambdaKaon_5p36TeV_Pythia8TuneCP5_EvtGen_cfi.py.py
@@ -4,14 +4,14 @@
 #GenXsecAnalyzer:
 #------------------------------------
 #Before Filter: total cross section = 5.728e+10 +- 0.000e+00 pb
-#Filter efficiency (taking into account weights)= (97) / (100000) = 9.700e-04 +- 9.844e-05
-#Filter efficiency (event-level)= (97) / (100000) = 9.700e-04 +- 9.844e-05    [TO BE USED IN MCM]
+#Filter efficiency (taking into account weights)= (162) / (100000) = 1.620e-03 +- 1.272e-04
+#Filter efficiency (event-level)= (162) / (100000) = 1.620e-03 +- 1.272e-04    [TO BE USED IN MCM]
 
-#After filter: final cross section = 5.556e+07 +- 5.639e+06 pb
+#After filter: final cross section = 9.279e+07 +- 7.285e+06 pb
 #After filter: final fraction of events with negative weights = 0.000e+00 +- 0.000e+00
-#After filter: final equivalent lumi for 1M events (1/fb) = 1.800e-05 +- 1.827e-06
+#After filter: final equivalent lumi for 1M events (1/fb) = 1.078e-05 +- 8.461e-07
 
-# 0.004009 sec/output event, 306 kB/output event
+# 0.003578 sec/output event 278 kB/output event
 
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
@@ -69,8 +69,6 @@ generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
 OmegaDaufilter = cms.EDFilter("PythiaMomDauFilter",
                                ParticleID = cms.untracked.int32(3334),
                                MomMinPt = cms.untracked.double(0.0),
-                               MomMinEta = cms.untracked.double(-2.4),
-                               MomMaxEta = cms.untracked.double(2.4),
                                DaughterIDs = cms.untracked.vint32(3122, -321),
                                NumberDaughters = cms.untracked.int32(2),
                                NumberDescendants = cms.untracked.int32(0),
